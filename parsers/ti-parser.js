e1b1fafe5f90cf73b820c07537e9ae960ca529fa
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-10-18 22:15:45 -0700

    Add git ignore.

--- parsers/ti-parser.js
+++ parsers/ti-parser.js
@@ -3,7 +3,7 @@ const json2csv = require('json2csv');
 
 const TIMEOUT = 3000;
 
-const SUMMARY_COLS = ["Brand Id", "Market Id", "Channel Id", "Skus", "Stores", "Combinations"];
+const SUMMARY_COLS = ["Brand Id", "Market Id", "Channel Id", "Skus", "Stores", "Combinations", "Total TI", "Total Rounded TI"];
 const RESULT_COLS = ["SKU", "Store", "Cycle Stock", "Safety Stock", "Vis Min Delta", "TI", "Rounded TI", "Capped", "Override"];
 const FIELDS = ["skuId", "storeId", "cycleStock", "safetyStock", "visMinDelta", "targetInventory", "roundedTargetInventory", "capped", "overrideType"];
 
@@ -29,22 +29,26 @@ class TIParser {
     }
 
     report(data) {
-    	var report = { summary: {}, results: {} };
+    	var report = { summary: {}, results: {}, csv: "" };
 
         var skus = new Set(), stores = new Set();
+        var totalTi = 0, totalRoundedTi = 0;
+
         for (var i = 0; i < data.skuStore.length; i++) {
             skus.add(data.skuStore[i].skuId);
             stores.add(data.skuStore[i].storeId);
+
+            totalTi += data.skuStore[i].targetInventory;
+            totalRoundedTi += data.skuStore[i].roundedTargetInventory;
         }
 
     	report.summary.columns = SUMMARY_COLS;
-    	report.summary.values = [data.brandId, data.marketId, data.channelId, skus.size, stores.size, data.skuStore.length]
+    	report.summary.values = [data.brandId, data.marketId, data.channelId, skus.size, stores.size, data.skuStore.length, totalTi, totalRoundedTi]
 
         report.results.columns = RESULT_COLS;
         report.results.values = data.skuStore.map(ss => [ss.skuId, ss.storeId, ss.cycleStock, ss.safetyStock, ss.visMinDelta, ss.targetInventory, ss.roundedTargetInventory, ss.capped, ss.overrideType]).sort();
 
-        var result = json2csv({ data: data.skuStore, fields: FIELDS })
-        console.log(result);
+        report.csv = json2csv({ data: data.skuStore, fields: FIELDS })
 
     	return report;
     }
